16648f3a287b63a1682533772202e1d253af0317
vuhieptran95 <vuhiep.tran95@gmail.com>
2019-01-13 18:25:18 +0700

    Update FirebaseConfig.js

--- config/FirebaseConfig.js
+++ config/FirebaseConfig.js
@@ -1,15 +1,1 @@
-const firebase = require("firebase");
 
-var config = {
-  apiKey: "AIzaSyBIG6hlZT8NYMQwQfAxVJ-VQ1VZkwm1cQk",
-  authDomain: "thuexequytam-firebase.firebaseapp.com",
-  databaseURL: "https://thuexequytam-firebase.firebaseio.com",
-  projectId: "thuexequytam-firebase",
-  messagingSenderId: "82777450298"
-};
-
-firebase.initializeApp(config);
-const Db = firebase.firestore();
-Db.settings({ timestampsInSnapshots: true });
-
-module.exports = Db;
